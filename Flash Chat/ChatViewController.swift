a8c957a5e7e8fea5b35df65f2098f85e3093306b
Jeff Umandap <jeffumandap@Jeffs-MacBook-Pro.local>
2021-03-11 23:54:07 +0800

    Initial sending/saving message to firebase completed.

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -151,8 +151,33 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     @IBAction func sendPressed(_ sender: AnyObject) {
         
-        
+
+        messageTextfield.endEditing(true)
+
         //TODO: Send the message to Firebase and save it in our database
+        messageTextfield.isEnabled = false
+        sendButton.isEnabled = false
+        
+        let messagesDB = Database.database().reference().child("Messages")
+        
+        let messageDictionary = ["Sender": Auth.auth().currentUser?.email,
+                                 "MessageBody": messageTextfield.text!]
+        
+        messagesDB.childByAutoId().setValue(messageDictionary) {
+            (error, reference) in
+            
+            if error != nil {
+                print(error!)
+            } else {
+                print("Send successfully!")
+                
+                self.messageTextfield.isEnabled = true
+                self.sendButton.isEnabled = true
+                
+                self.messageTextfield.text = ""
+            }
+        }
+        
         
         
     }
